5f6ef9f9b09df3ac67e595da857c15227d27c61a
click-to-generate-name <avery2718.jpi@gmail.com>
2022-04-24 21:28:46 +0200

    Redo rain_pngs to allow axes config
    Show example image to allow configuration of which axis (x,y,z)
    corresponds to which plane (ax,cr,sg), and what the rotation of each
    plane should be

--- rain_pngs.py
+++ rain_pngs.py
@@ -3,64 +3,95 @@ import numpy as np
 import nibabel as nib
 from scipy import ndimage
 
-# Get all the paths to the image directories (dirs should contain only niftis)
-# TODO: get them all
+# TODO: Go per original dataset directory
 file_path = ''
 
 # Load the nifti image data
 niimg_array = nib.load(file_path).get_fdata()
 
-# If the image is 4D, i.e. a time series, only take the first volume (- this balances the amount of high and low detail images and is simpler to implement)
+# If the image is 4D, i.e. a time series, only take the first volume
+# (balances the amount of high and low detail images 
+# and is simpler to implement)
 if len(niimg_array.shape) == 4:
     niimg_array = niimg_array[:,:,:,0]
 
-# Dimensions of array (num of coronal, axial, sagittal slices)
-cr_dim, ax_dim, sg_dim = niimg_array.shape
+# Show one slice for visual inspection
+fig, axs = plt.subplots(1,3)
+axs[0].imshow(niimg_array[100,:,:], cmap='gray')
+axs[0].set_title('0')
+axs[1].imshow(niimg_array[:,100,:], cmap='gray')
+axs[1].set_title('1')
+axs[2].imshow(niimg_array[:,:,100], cmap='gray')
+axs[2].set_title('2')
+plt.show()
 
-def cut_in_plane(plane, cut, arr):
+# After inspection of x,y,z images, enter which corresponds to each axis
+# and how much to roate each
+axs_dict = {
+    'ax':eval(input('Axial axis (0,1,2): ')),
+    'cr':eval(input('Coronal axis (0,1,2): ')),
+    'sg':eval(input('Sagittal axis (0,1,2): '))
+}
+rot_dict = {
+    'ax':eval(input('Rotate axial images ccw by [deg]: ')),
+    'cr':eval(input('Rotate coronal images ccw by [deg]: ')),
+    'sg':eval(input('Rotate sagittal images ccw by [deg]: '))
+}
+
+# Dimensions of array (num of axial, coronal, sagittal slices)
+dims = niimg_array.shape
+ax_len = dims[axs_dict['ax']]
+cr_len = dims[axs_dict['cr']]
+sg_len = dims[axs_dict['sg']]
+
+def cut_in_plane(axis, cut, arr):
     '''
-    Cut a nifti image array along a specified plane (axial, coronal, sagittal). Can cut a single slice, or a section. The array must be 3 dimensional.
+    Cut a nifti image array along a specified axis (x, y, z, corresponding in any order to axial, coronal, sagittal planes). Can cut a single slice, or a section. The array must be 3 dimensional.
 
     args:
-        plane (string): Plane to cut along, must be one of 'ax', 'cr', 'sg'
-        cut (int or tuple of int): index at which to cut or (start, stop) indexes of cut
-        arr (list of float): Niimg array (3D) to be cut
+        axis (int):
+        cut (int or tuple of int):
+        arr (list of float):
     
     returns:
-        cut_arr (list of float): Niimg array cut as specified. Empty array if input is invalid
+        cut_arr (list of float):
     '''
     cut_arr = np.array([])
-    if plane=='ax':
-        cut_arr = arr[:,cut[0]:cut[1],:] if type(cut) is tuple else arr[:,cut,:]
-    if plane=='cr':
+    if axis==0:
         cut_arr = arr[cut[0]:cut[1],:,:] if type(cut) is tuple else arr[cut,:,:]
-    if plane=='sg':
+    if axis==1:
+        cut_arr = arr[:,cut[0]:cut[1],:] if type(cut) is tuple else arr[:,cut,:]
+    if axis==2:
         cut_arr = arr[:,:,cut[0]:cut[1]] if type(cut) is tuple else arr[:,:,cut]
     return cut_arr
 
 # Select a part in the middle in each direction
-# (which part determined visually on one example)
-ax_arr = cut_in_plane('ax', (ax_dim//2, ax_dim-ax_dim//6), niimg_array)
-cr_arr = cut_in_plane('cr', (cr_dim//4, cr_dim-cr_dim//5), niimg_array)
-sg_arr = cut_in_plane('sg', (sg_dim//4, sg_dim-sg_dim//4), niimg_array)
+# (which part determined visually on one example, hardcoded)
+ax_arr = cut_in_plane(axs_dict['ax'], (ax_len//2, ax_len-ax_len//6), 
+    niimg_array)
+cr_arr = cut_in_plane(axs_dict['cr'], (cr_len//4, cr_len-cr_len//5), 
+    niimg_array)
+sg_arr = cut_in_plane(axs_dict['sg'], (sg_len//4, sg_len-sg_len//4), 
+    niimg_array)
 
-# Coronal and sagittal images must be rotated
-sg_arr = ndimage.rotate(sg_arr, 90)
-cr_arr = ndimage.rotate(cr_arr, 180)
+# Rotate images as required
+ax_arr = ndimage.rotate(ax_arr, rot_dict['ax'], (axs_dict['cr'],axs_dict['sg']))
+cr_arr = ndimage.rotate(cr_arr, rot_dict['cr'], (axs_dict['ax'],axs_dict['sg']))
+sg_arr = ndimage.rotate(sg_arr, rot_dict['sg'], (axs_dict['ax'],axs_dict['cr']))
 
 # TODO: Put this in a function for readability
 # Slice em and save em
-print('axial')
-for slice in np.linspace(0, ax_arr.shape[1], num=10, dtype=np.uint16, endpoint=False):
+print('Generating axial images...')
+for slice in np.linspace(0, min(ax_arr.shape), num=10, dtype=np.uint16, endpoint=False):
     plt.imsave('Images/ax_{}_someid.png'.format(slice), 
-        ax_arr[:,slice,:], cmap='gray')                    # axial
+        cut_in_plane(axs_dict['ax'], slice, ax_arr), cmap='gray')                    # axial
 
-print('coronal')
-for slice in np.linspace(0, cr_arr.shape[0], num=10, dtype=np.uint16, endpoint=False):
+print('Generating coronal images...')
+for slice in np.linspace(0, min(cr_arr.shape), num=10, dtype=np.uint16, endpoint=False):
     plt.imsave('Images/cr_{}_someid.png'.format(slice), 
-        cr_arr[slice,:,:], cmap='gray')                  # coronal
+        cut_in_plane(axs_dict['cr'], slice, cr_arr), cmap='gray')                  # coronal
 
-print('sagittal')
-for slice in np.linspace(0, sg_arr.shape[2], num=10, dtype=np.uint16, endpoint=False):
+print('Generating sagittal images...')
+for slice in np.linspace(0, min(sg_arr.shape), num=10, dtype=np.uint16, endpoint=False):
     plt.imsave('Images/sg_{}_someid.png'.format(slice), 
-        sg_arr[:,:,slice], cmap='gray')                 # sagittal
+        cut_in_plane(axs_dict['sg'], slice, sg_arr), cmap='gray')                 # sagittal
