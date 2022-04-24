3a585ca9ec795475b473c9b3e2c05dd80c341955
click-to-generate-name <avery2718.jpi@gmail.com>
2022-04-24 17:16:35 +0200

    Handle 4D niimgs when generating pngs
    Delete files that remove 4D image files, and add code to script that
    generates pngs (rain_pngs) to extract first volume from 4D time series.

--- rain_pngs.py
+++ rain_pngs.py
@@ -10,8 +10,14 @@ file_path = 'Datasets/GAZE_PRF/sub-001_ses-03_anat_sub-001_ses-03_acq-ADNI_run-2
 # Load the nifti image data
 niimg_array = nib.load(file_path).get_fdata()
 
+# If the image is 4D, i.e. a time series, only take the first volume (- this balances the amount of high and low detail images and is simpler to implement)
+if len(niimg_array.shape) == 4:
+    niimg_array = niimg_array[:,:,:,0]
+
 # Dimensions of array (num of coronal, axial, sagittal slices)
 cr_dim, ax_dim, sg_dim = niimg_array.shape
+
+# TODO: Implement cut_in_plane function
 
 # Select a part in the middle in each direction
 # (which part determined visually on one example)
@@ -23,6 +29,7 @@ sg_arr = niimg_array[:, :, sg_dim//4 : sg_dim-sg_dim//4]
 sg_arr = ndimage.rotate(sg_arr, 90)
 cr_arr = ndimage.rotate(cr_arr, 180)
 
+# TODO: Put this in a function for readability
 # Slice em and save em
 print('axial')
 for slice in np.linspace(0, ax_arr.shape[1], num=10, dtype=np.uint16, endpoint=False):
